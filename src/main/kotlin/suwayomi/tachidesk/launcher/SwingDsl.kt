84df1b7fc6878cc3af09c45c97545c37b52da343
Aria Moradi <aria.moradi007@gmail.com>
2023-01-15 13:05:47 +0330

    fix copyright notice

--- src/main/kotlin/suwayomi/tachidesk/launcher/SwingDsl.kt
+++ src/main/kotlin/suwayomi/tachidesk/launcher/SwingDsl.kt
@@ -1,10 +1,12 @@
+package suwayomi.tachidesk.launcher
+
 /*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-
-package suwayomi.tachidesk.launcher
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
