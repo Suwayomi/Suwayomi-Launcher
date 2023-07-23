b187c93a618e3cb578bbf64f4820f044670f2862
Syer10 <syer10@users.noreply.github.com>
2023-07-22 20:18:37 -0400

    Add missing licenses

--- src/main/kotlin/suwayomi/tachidesk/launcher/ui/ServerIpAndPortBindings.kt
+++ src/main/kotlin/suwayomi/tachidesk/launcher/ui/ServerIpAndPortBindings.kt
@@ -1,5 +1,13 @@
 package suwayomi.tachidesk.launcher.ui
 
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.filter
